2b7aafca424c467efcb13e717947e83ddafd648e
CGX11 <charlie@hazardinteractive.com>
2017-01-17 05:37:59 +0000

    Removed text render node, figuring out ui architecture

--- SFMLProj/TestScene.cpp
+++ SFMLProj/TestScene.cpp
@@ -1,7 +1,6 @@
 #include "TestScene.h"
 #include "Game.h"
 #include "NodeFactory.h"
-#include "TextRenderNode.h"
 
 TestScene::TestScene() {}
 TestScene::~TestScene() {}
@@ -13,10 +12,6 @@ void TestScene::onStart()
 	_game->addSceneNode(NodeFactory::createAsteroid(10, 10));
 
 	_game->addSceneNode(NodeFactory::createPlayerNode());
-
-	auto testNode = new SceneNode;
-	testNode->addChild(new TextRenderNode("Fonts//kenvector_future.ttf", "Hello, World."));
-	_game->addSceneNode(testNode);
 
 	Scene::onStart();
 }
